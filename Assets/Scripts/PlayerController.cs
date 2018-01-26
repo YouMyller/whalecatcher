82b40a7f0f12e28d0ab19e4ad855b5e5f4e2f659
Niininen Touko TTK17SP <toukoniininen@kamk.fi>
2018-01-26 22:38:27 +0200

    Player controller script added and whale script modified.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject soundWave;
+
+    [SerializeField]
+    private float soundWaveSpeed;
+
+    //Rigidbody2D rb;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Painaa mouse");
+            /*
+            var mousePos = Input.mousePosition;
+            var objectPos = Camera.current.ScreenToWorldPoint(mousePos);
+            Instantiate(soundWave, objectPos, Quaternion.identity);
+
+            Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = (Input.mousePosition - sp).normalized;
+            rb.AddForce(dir * 2);
+            GameObject spawned = Instantiate(soundWave, dir, transform.rotation);
+            spawned.GetComponent<Rigidbody2D>().AddForce(new Vector2(dir, dir));*/
+
+            Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y));
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y + 1);
+            Vector2 direction = target - myPos;
+            direction.Normalize();
+            GameObject spawned = (GameObject)Instantiate(soundWave, myPos, Quaternion.identity);
+            spawned.GetComponent<Rigidbody2D>().velocity = direction * 50;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Whale.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whale : MonoBehaviour
+{
+    public GameObject whale;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    public Transform target;
+    bool towardsSound = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SoundWave")
+        {
+            Debug.Log("koskettaa");
+            towardsSound = true;
+        }
+    }
+
+    private void Move()
+    {
+        if (towardsSound == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed* Time.deltaTime);
+        }
+
+        else
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        
+    }
+}
