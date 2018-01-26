9e6647a9dbfddb3062b444144a38af3ec4401a9d
Myllylä Juuso TTK17SP <juusomyllyla1@kamk.fi>
2018-01-26 22:30:33 +0200

    Random spawning & claw rotation start

---
+++ Assets/ClawGrab.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawGrab : MonoBehaviour {
+
+    public bool rotate = false;
+
+    public float rotationSpeed = 5;
+    public float rotationTime;
+    public float beginTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (rotate == true)
+        {
+            rotationTime -= Time.deltaTime;
+            transform.Rotate(Vector3.forward * rotationSpeed);
+            //make the object rotate with the claw
+            if (rotationTime <= 0)
+            {
+                rotate = false;
+            }
+        }
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BlueWhale" || collision.gameObject.tag == "KillerWhale" || collision.gameObject.tag == "Narwhal")
+        {
+            rotate = true;
+            rotationTime = beginTime;
+            collision.gameObject.tag = "Null";
+        }
+    }
+}
